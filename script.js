a7d9d800b3239516013a18dc7615428418031499
Kidar-kush <kidarkush@yahoo.com>
2025-11-27 20:29:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+import check from '/img/pizza.png'
+import check from '/img/star.png'
+import check from '/img/coke.png'
+import check from '/img/palm1.png'
+import check from '/img/burger.png'
+import check from '/img/chips.png'
+
+
+AOS.init({
+    duration: 1000,
+    offset:100,
+});
+
